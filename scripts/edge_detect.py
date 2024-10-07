8d0b8698713590d47a9ceed0189b340cb58dba27
Kolad2 <koladik@gmail.com>
2024-10-08 01:45:48 +0300

    update edge_detect

--- scripts/edge_detect.py
+++ scripts/edge_detect.py
@@ -1,1 +1,18 @@
-#from rockedgesdetectors import M
+import cv2
+from rockedgesdetectors import ModelGPU
+
+big_frame = cv2.imread("image.png")
+model = ModelGPU("../models/RCFcheckpoint_epoch12.pth")
+edges = model.get_model_edges(big_frame)
+#
+cv2.namedWindow("wnd", cv2.WINDOW_NORMAL)
+cv2.resizeWindow('wnd', 800, 600)
+cv2.imshow("wnd", big_frame)
+cv2.waitKey(0)
+#
+cv2.namedWindow("wnd", cv2.WINDOW_NORMAL)
+cv2.resizeWindow('wnd', 800, 600)
+cv2.imshow("wnd", edges)
+cv2.waitKey(0)
+
+
