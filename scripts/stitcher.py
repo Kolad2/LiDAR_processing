9daf20ca186437b6b7092e12a6ba6479850e2cfc
Kolad2 <koladik@gmail.com>
2024-10-07 22:04:40 +0300

    stitcher update

---
+++ scripts/stitcher.py
@@ -0,0 +1,13 @@
+import cv2
+from orthostitcher import CV2VideoStitcher
+
+video_path = "../renders/output.mp4"
+
+
+stitcher = CV2VideoStitcher(video_path)
+frame = stitcher.stitch()
+
+cv2.imwrite("image.png", frame)
+
+
+
