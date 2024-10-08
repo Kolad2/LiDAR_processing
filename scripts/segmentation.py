4228fd5ea6707a9ba9e2bca182681d38d74de23c
Kolad2 <koladik@gmail.com>
2024-10-08 03:58:00 +0300

    scripts update 2

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -1,11 +1,15 @@
 import cv2
+import numpy as np
 from pyrocksegmentation import RockImageSegmentator
 from rockedgesdetectors import ModelGPU
 
 image = cv2.imread("image.png")
 
-model = ModelGPU("../models/RCFcheckpoint_epoch12.pth")
-edges_weighted = model.get_model_edges(image)
+# model = ModelGPU("../models/RCFcheckpoint_epoch12.pth")
+# edges_weighted = model.get_model_edges(image)
+# np.save('edges_weighted.npy', edges_weighted)
+
+edges_weighted = np.load('edges_weighted.npy')
 
 segmentator = RockImageSegmentator(image=image, edges_weighted=edges_weighted)
 segmentator.run()
