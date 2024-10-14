389451db8e468524412273b9058328d9fe6fd7cb
Kolad2 <koladik@gmail.com>
2024-10-14 03:30:15 +0300

    update sripts

--- scripts/statistic_analiz.py
+++ scripts/statistic_analiz.py
@@ -1,15 +1,39 @@
 import numpy as np
-from pyrockstats.distrebutions import weilbull, lognorm, paretoexp
-
-
-weilbull.Weilbull(2, 2)
+import matplotlib.pyplot as plt
+from scipy import stats as st
+from pyrockstats.distrebutions import weibull, lognorm, paretoexp, gengamma
+from pyrockstats import ecdf
 
 
 s = np.load('sampling.npy')
-theta_1 = lognorm.fit_mle(s)
-# theta_2 = weilbull.fit_mle(s)
-# theta_3 = paretoexp.fit_mle(s)
-#
-print(theta_1)
-# print(theta_2)
-# print(theta_3)
+
+x, eF = ecdf(s)
+x_min = np.min(x)
+x_max = np.max(x)
+
+model = {
+	"lognorm": lognorm,
+	"weibull": weibull,
+	"paretoexp": paretoexp,
+	"gengamma": gengamma,
+}
+
+theta, distribution, cdf = {}, {}, {}
+for name in model:
+	theta[name] = model[name].fit(s)
+	print(name, theta[name])
+	distribution[name] = model[name](*theta[name])
+	cdf[name] = distribution[name].cdf(x)
+
+fig = plt.figure(figsize=(16, 4))
+ax = [fig.add_subplot(1, 1, 1)]
+ax[0].plot(x, eF, color='black')
+ax[0].plot(x, cdf["lognorm"], color='red')
+ax[0].plot(x, cdf["weibull"], color='green')
+ax[0].plot(x, cdf["paretoexp"], color='red')
+ax[0].plot(x, cdf["gengamma"], color='blue')
+ax[0].set_xlim([x_min, x_max])
+ax[0].set_xscale('log')
+plt.show()
+
+
