f698cbe68ac0f23a474461cf83ab0a1413ef835c
kolad <koladik@gmail.com>
2024-11-05 21:59:09 +0300

    module update

--- requirements.md
+++ requirements.md
@@ -3,4 +3,5 @@ pip install opencv-python
 pip install scikit-image
 pip install matplotlib
 pip install rasterio
+pip install pathlib
 ```
---
+++ scripts/tif_merge.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from rocknetmanager.tools import mosaic_merge
+
+path_to_images = Path("/media/kolad/HardDisk/train_data/Matteo_database/Site_A/images")
+
+mosaic_merge(path_to_images)
+
+# geotif2geotif_standart()
+
